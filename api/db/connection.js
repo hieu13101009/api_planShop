3389ca2f87a98e35757490f6666d92171e6e030a
Hieu <hieund@runsystem.net>
2020-06-01 10:12:53 +0700

    container auto rs with mp2 ss

--- api/db/connection.js
+++ api/db/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.33.12:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://192.168.33.21:27017/test', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("we're connected!");
+    console.log("333333");
 });
 module.export = db
--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     apps : [{
-      name: 'docker_web_app',
+      name: 'app',
       script: './app.js',
       watch: true,
+      watch_delay: 1000,
+      "watch_options": {
+          usePolling: true
+      },
       ignore_watch : ['node_modules'],
     }]
   };
