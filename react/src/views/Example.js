bc40bb91736aaed3270745635b9c07ca1379e3a8
Hieu <hieund@runsystem.net>
2020-06-11 16:22:56 +0700

    react add example hook

--- react/src/views/Example.js
+++ react/src/views/Example.js
@@ -1,23 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
-function Example() {
-    const [count, setCount] = useState(0);
+const App = () => {
+const [newTodo, setNewTodo] = useState('');
+const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        document.title = `You clicked ${count} times`;
+const onNewTodoChange = useCallback(
+    (event) => {
+    setNewTodo(event.target.value)
+    console.log(event.target.value)
+    },
+    [],
+)
+const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    if (!newTodo.trim()) return;
+    console.log('form was submitted')
+    setTodos([
+    {
+        id: todos.length ? todos[0].id + 1 : 1,
+        content: newTodo,
+        done: false
+    },
+    ...todos
+
+    ])
+    setNewTodo('');
+}, [newTodo, todos]);
+
+const addTodo = useCallback((todo, index) => (event) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1, {
+    ...todo,
+    done: !todo.done
+    })
+    setTodos(newTodos)
+}, [todos])
+
+const removeTodo = useCallback((todo) => (event) => {
+    setTodos(todos.filter(otherTodo => otherTodo !== todo));
+}, [todos]);
+
+const markAllDone = useCallback(() => {
+    // create a copy of the array
+    // create a copy of each of the items
+        // update the done property to be true on each of the new items
+    const updatedTodos = todos.map(todo => {
+    return {
+        ...todo,
+        done: true,
+    };
     });
-    
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
-            Click me
-        </button>
-        <button onClick={() => setCount(0)}>
-            refresh
-        </button>
-        </div>
-    );
+    setTodos(updatedTodos);
+}, [todos]);
+
+useEffect(() => {
+    console.log('todos', todos)
+}, [todos])
+
+return (
+    <div className="App">
+    <form onSubmit={formSubmitted}>
+        <label>Enter a Todo: </label>
+        <input
+        id="newTodo"
+        name="newTodo"
+        value={newTodo}
+        onChange={onNewTodoChange}
+        />
+    <button>add Todo</button>
+    </form>
+    <button onClick={markAllDone}>Mark All Done</button>
+    <ul>
+        {todos.map((todo, index) => (
+        <li 
+            key={todo.id}>
+            <input
+            checked={todo.done}
+            type="checkbox"
+            onChange={addTodo(todo, index)}
+            />
+            <span className={todo.done ? 'done': ''}>{todo.content}</span>
+            <button onClick={removeTodo(todo)}>Remove Todo</button>
+        </li>
+        ))}
+    </ul>
+    </div>
+);
 }
 
-export default Example;
+export default App;
